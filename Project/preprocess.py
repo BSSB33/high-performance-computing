92a6645a855bdf77555e636167f840f3cdd8976b
Gábor Vitrai <vitraig@gmail.com>
2021-11-16 12:48:42 +0100

    Project Pre-processing V2

--- Project/preprocess.py
+++ Project/preprocess.py
@@ -1,8 +1,8 @@
 import re
 import string
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import TweetTokenizer
+import nltk
+from nltk.tokenize import word_tokenize
 
 def import_book(path):
     f = open(path, "r", encoding="utf8")
@@ -16,26 +16,28 @@ def export_sentences(path, sentences):
 def process_sentence(sentences):
     processed_sentences = []
     for sentence in sentences:
-        # Remove punctuation and extra characters
         sentence = re.sub('[' + string.punctuation + ']', '', sentence)
-        sentence = re.sub('[“”]*', '', sentence)
+        sentence = re.sub('[“”’—]*', '', sentence)
         sentence = re.sub("^\s+|\s+$", "", sentence, flags=re.UNICODE)
         processed_sentences.append(sentence)
     return processed_sentences
 
 def tokenize_sentences(sentences):
     tokenized_sentences = []
-    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 
     for sentence in sentences:
-        sentence_tokens = tokenizer.tokenize(sentence)
-        tokenized_sentences.append(sentence_tokens)
+        tokenized_sentence = word_tokenize(sentence)
+        for word in tokenized_sentence:
+            tokenized_sentences.append(word)
     return tokenized_sentences
 
 if __name__ == "__main__":
+    nltk.download('punkt')
+
     book = import_book("alice.txt")
     sentences = book.split(".")
     sentences = process_sentence(sentences)
     tokenized_sentences = tokenize_sentences(sentences)
+
     export_sentences("alice_sentences.txt", sentences)
     export_sentences("alice_tokenized_sentences.txt", tokenized_sentences)
