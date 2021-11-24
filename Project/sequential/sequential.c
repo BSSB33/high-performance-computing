4ac22b50a1b3aeb6cde5c93b80af5d9af4733da9
Nagy Laszlo <nagylaszlolacko@gmail.com>
2021-11-24 11:00:10 +0100

    added sequential implementation of dictionary processing

---
+++ Project/sequential/sequential.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "uthash.h"
+
+struct dict_item
+{
+    char word[255]; // we use this as key for the hash table
+    int nr;
+    UT_hash_handle hh; // makes the structure hashable
+};
+
+struct dict_item *my_dict = NULL;
+
+void add_item(char *word)
+{
+    struct dict_item *s;
+
+    HASH_FIND_STR(my_dict, word, s);
+    if (s == NULL)
+    {
+        s = (struct dict_item *)malloc(sizeof *s);
+        // s->word = word;
+        strcpy(s->word, word);
+        HASH_ADD_STR(my_dict, word, s);
+        s->nr = 1;
+    }
+    else
+    {
+        s->nr++;
+    }
+}
+
+void print_dict(int max_items)
+{
+    struct dict_item *s;
+    if (max_items == -1)
+    {
+
+        for (s = my_dict; s != NULL; s = (struct dict_item *)(s->hh.next))
+        {
+            printf("word: %s - nr: %d\n", s->word, s->nr);
+        }
+    }
+    else
+    {
+        int j = 1;
+        for (s = my_dict; s != NULL && max_items > 0; s = (struct dict_item *)(s->hh.next))
+        {
+            printf("%d. word: %s - nr: %d\n", j, s->word, s->nr);
+            max_items--;
+            j++;
+        }
+    }
+}
+
+/*
+    Comparison function for sorting by number of occurences
+*/
+int nr_sort(struct dict_item *a, struct dict_item *b)
+{
+    return (b->nr - a->nr);
+}
+
+void sort_by_nr()
+{
+    HASH_SORT(my_dict, nr_sort);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: <program> <input-file> <0|1>"); // 0 - words, 1 - sentences
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+    struct timeval t1, t2;
+    gettimeofday(&t1, NULL);
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (atoi(argv[2]) == 0)
+    {
+        while ((read = getline(&line, &len, fp)) != -1)
+            add_item(line);
+    }
+    else
+    {
+        while ((read = getline(&line, &len, fp)) != -1)
+        {
+            char *word = strtok(line, " ");
+            while (word)
+            {
+                add_item(word);
+                word = strtok(NULL, " ");
+            }
+        }
+    }
+
+    sort_by_nr();
+    gettimeofday(&t2, NULL);
+    float elapsed_time = (t2.tv_sec - t1.tv_sec) + 1e-6 * (t2.tv_usec - t1.tv_usec);
+    printf("\nCompleted in %0.6f seconds", elapsed_time);
+    printf("\n\n Resulting dictionary:\n");
+    print_dict(30);
+
+    fclose(fp);
+    free(line);
+
+    return 0;
+}
