84361b77a354bb6b719d79dcdd6e90bca1855680
Gábor Vitrai <vitraig@gmail.com>
2021-12-10 19:47:45 +0100

    Sending Array of Struct tested and debugged

--- Project/proj.c
+++ Project/proj.c
@@ -8,6 +8,8 @@ const int MAX_STRING = 100000;
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <err.h>
+#include <stddef.h>
+#include <stdio.h>
  
 /* following code assumes all file operations succeed. In practice,
  * return codes from open, close, fstat, mmap, munmap all need to be
@@ -78,28 +80,59 @@ int read_file_line(const char *path, int line_no, int end_line_no, char *output)
 	return ret;
 }
 
+
 int main(void) { 
-		char *filename = "/home/gabor.vitrai/proj/alice/alice_sentences.txt";
+		//char *filename = "/home/gabor.vitrai/proj/alice/alice_sentences.txt";
         int comm_sz; /* Number of processes */ 
         int my_rank; /* My process rank */ 
 
         MPI_Init(NULL, NULL); 
         MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
-        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+		MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+		struct node {
+			char word[25];	//key
+			int nr;			//value
+		};
+
+		
+		const int nitems = 2;
+		int blocklengths[2] = {25,1};
+		MPI_Datatype types[2] = {MPI_CHAR, MPI_INT};
+		MPI_Datatype DictType;
+		MPI_Aint offsets[2];
+		offsets[0] = offsetof(struct node, word);
+		offsets[1] = offsetof(struct node, nr);
+		MPI_Type_create_struct(nitems, blocklengths, offsets, types, &DictType);
+		MPI_Type_commit(&DictType);
 
         if (my_rank == 0) { // Main
-        	//TODO Broadcast first line of the files and open every file from n+1 (skipping the first line - alias number of rows)
-			char text[MAX_STRING];
-        	read_file_line(filename, 900, 902, text);
-			printf("%s\n", text);
+
+			struct node data[5] = {{"alice", 5}, {"bob", 10}, {"charlie", 15}, {"david", 20}, {"eve", 25}};
+
+			int size = sizeof(data) / sizeof(data[0]);
+			for (int q = 1; q < comm_sz; q++) { 
+				MPI_Send(&size, sizeof(size), MPI_INT, q, 0, MPI_COMM_WORLD); 
+				printf("MAIN: Process %d - Sending size count: %d, to process %d\n", my_rank, size, q);
+				
+				//MPI_Send(&data, sizeof(data), MPI_BYTE, q, 0, MPI_COMM_WORLD);
+
+				MPI_Send(&data, size, DictType, q, 0, MPI_COMM_WORLD);
+				printf("MAIN: Process %d - Sending struct to process %d\n", my_rank, q);
+			}
         } 
         else { // Childs
+		
+			int size;
+			MPI_Recv(&size, sizeof(size), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            printf("CHILD: Process %d - Size recieved: %d!\n", my_rank, size);
 
+			struct node data[size];
+
+			//MPI_Recv(&data, sizeof(data), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+			MPI_Recv(&data, size, DictType, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+			printf("CHILD: Process %d - Struct recieved! %d. word: %s\n", my_rank, my_rank-1, data[my_rank-1].word);
         }
         MPI_Finalize(); 
         return 0; 
 } /* main */
-
-// Problems:
-// 2. Child rank is always 4
-// 3. Big text input is still not working perfectly
